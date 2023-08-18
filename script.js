7fd002ed9343f4e9440307e59d2d801f5454d880
Iroh-The-Wise <charlesvill@g.ucla.edu>
2023-08-18 16:31:48 -0700

    completed basic core functionality with a single gameplay stage. nested all variables in either modules or factory to limit globally scoped variables

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const Player = (team) => {
 // console.log(player2.getTeam());
 
 const gameboard = (()=> {
-    const boardArr = [0,1,2,3,4,5,6,7,8];
+    
     
     const board = document.querySelector(".board")
 
@@ -33,31 +33,125 @@ const gameboard = (()=> {
 
 })();
 
+
 const gameLoop = (()=> {
     //call gameboard to build
-        //will need something here to connect the other object methods to this. 
-    //store square event listeners
+       
+    let turn = 0; 
+    const boardArr = [];
     const fields = document.querySelectorAll(".square");
+    const updateBoardArr = (dataIndex)=>{
+    let repeat = false;
+    //check for duplicate clicks
+
+        if(turn < 9 && boardArr[dataIndex] === undefined){ 
+            //generate the pair of position and player for each turn (index) in boardArr
+            repeat = false;
+            if(turn%2 === 0){
+                boardArr[dataIndex] = 'x';
+            }
+            else{
+                boardArr[dataIndex] = 'o';
+            }
+            
+           
+            turn++;
+            //call to change display
+            //check # turns and call checkwin
+            if(turn > 4 ){ 
+                CheckWin.assessArr(boardArr, turn);
+            }
+        }   
+        else if(boardArr[dataIndex] !== undefined){
+            console.log("repeat was detected, returning now.");
+            return boardArr;
+        }
+        
+        console.log(boardArr);
+    }
+    
     fields.forEach(element =>{
         element.addEventListener("click", (e)=> {
             //send event information to map to what square was pressed
             const dataIndex = Number(e.currentTarget.getAttribute("data-index"));
+            console.log(`data index is: ${dataIndex}`);
             mapBoardChoice(dataIndex);
             //check whos turn it is and mark choice
             //add player choice to array
             //add player turn 
-            //count number of turns 
-            //after three turns call module to check winner passing through info
-            //store button and call for game reset
+            
         })
     })
     const mapBoardChoice = (dataIndex) => {
         if(dataIndex === null){
             return;
         }
-
-        //send to add the choice to array
-        console.log(dataIndex);
+        updateBoardArr(dataIndex);
+        
         
     }
+
+    
+})();
+
+const CheckWin = (()=> {
+
+    let xCount = 0; 
+    let oCount = 0; 
+    const x = 'x';
+    const o =  'o';
+    let winner = false;
+    const winningPatterns = [
+        [0,1,2], 
+        [0,3,6], 
+        [0,4,8], 
+        [1,4,7], 
+        [2,4,6], 
+        [2,5,8], 
+        [3,4,5], 
+        [6,7,8]
+    ]; 
+    const assessArr = (boardArr, turn)=> {
+        for(let r = 0; r < winningPatterns.length; r++)
+        {
+            for(let c = 0, oCount = 0, xCount = 0; c < 3; c++)
+            {
+                let patternLocation = winningPatterns[r][c];
+                if( boardArr[patternLocation] === x){
+                    xCount++;
+                }
+                else if(boardArr[patternLocation] === o){
+                    oCount++; 
+                }
+            
+                if(xCount === 3){
+                    console.log("x wins");
+                    //xwins send method to trigger win
+                    //update display
+                    winner = true;
+                    return;
+                }
+                else if(oCount === 3){
+                    //xwins send method to trigger win
+                    console.log("o wins");
+                    //update display for owin
+                    winner = true;
+                    return;
+                }
+            }
+            
+            //use winning patterns to check the location of the boardArr and note if there are repeating 0s or xs 
+            
+        }
+        if(!winner && turn > 7){
+            console.log("There was a tie!");
+            //trigger tie
+            //update display
+        }
+        else{
+            console.log("keep playing");
+        }
+    }
+
+    return{assessArr: assessArr}
 })();
